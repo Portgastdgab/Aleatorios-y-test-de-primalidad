e1f3c05b0f159acc8d1cee545480304ccabd3869
Misash <misash@outlook.com>
2021-06-17 20:54:58 -0500

    Trivium CPNG and Miller

--- Numeros aleatorios/main.cpp
+++ Numeros aleatorios/main.cpp
@@ -1,9 +1,137 @@
+//
+// Created by misash on 17/06/21.
+//
+
+
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <NTL/ZZ.h>
 
 using namespace std;
+using namespace NTL;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+
+
+
+class Trivium{
+	
+private:
+	int s[288];
+	int IV[80],key[80];
+	int t1,t2,t3;
+	int bits;
+	
+	void generate_Key_and_IV(){
+		for(int i=0;i<80;i++){
+			IV[i]=rand()%2;
+			key[i]=rand()%2;
+		}
+	}
+	
+	void rotate(){
+		for(int j=93;j>=1;--j)
+			s[j]=s[j-1];
+		for(int j=177;j>=94;--j)
+			s[j]=s[j-1];
+		for(int j=288;j>=178;--j)
+			s[j]=s[j-1];
+		s[0]=t3;  s[93]=t1;  s[177]=t2;
+	}
+		
+		
+	void init(){
+		generate_Key_and_IV();
+		
+		for(int i=0;i<288;i++) s[i]= 0;
+		
+		for(int i=0;i<80;++i){
+			s[i]=key[i];
+			s[i+93]=IV[i];
+		}
+		for(int i=285;i<288;++i) s[i]=1;
+		
+		//rotate
+		for(int i=1;i<1152;i++){
+			t1 = ((s[65] ^ (s[90] & s[91]) )^ s[92] )^ s[170];
+			t2 = ((s[161] ^ (s[174] & s[175]) )^ s[176] )^ s[263];
+			t3 = ((s[242] ^ (s[285] & s[286]) )^ s[287] )^ s[68];
+			rotate();
+		}
+	}
+			
+	ZZ keyStream(){
+		ZZ z(0);
+		ZZ base10 (0);
+		
+		for(int i=0;i<bits;i++){
+			
+			t1 = s[65] ^ s[92];
+			t2 = s[161] ^ s[176];
+			t3 = s[242] ^ s[287];
+			
+			z = t1 ^ t2 ^ t3;
+			
+			t1 ^= s[90] ^ s[91] & s[170] ;
+			t2 ^= s[174] ^ s[175] & s[263] ;
+			t3 ^= s[285] ^ s[286] & s[68] ;
+			rotate();
+			base10 += z<<i;
+		}
+		
+		return base10;
+	}
+					
+public:
+						
+	Trivium(int bits){
+		this ->bits = bits;
+		init();
+	}
+	
+	ZZ PseudoRandom(){
+		ZZ n;
+		do{
+			n = keyStream();
+		}while(countBits(n)!=bits);
+		
+		return n;
+	}
+		int countBits(ZZ n){
+			int count =0;
+			while(n != 0){
+				count++;
+				n>>=1;
+			}
+			return count;
+		}
+				
+};
+
+
+
+/*----------------------------------------------*/
+
+
+				
+int main() {
+
+	
+	srand(time(0));
+	
+	int bits = 2048;
+	ZZ n;
+	Trivium test(bits);
+	n= test.PseudoRandom();
+	
+	cout<<"\nnumero: "<<n<<endl;
+	cout<<"\nbits: "<<test.countBits(n);
+	
+	cout<<endl;
+	
+	
+	
 }
+
+				
+				
--- Test de primalidad/main.cpp
+++ Test de primalidad/main.cpp
@@ -1,9 +1,102 @@
+
 #include <iostream>
+#include <NTL/ZZ.h>
 
 using namespace std;
+using namespace NTL;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+
+
+/*----------Funciones-----------------*/
+
+
+ZZ module(ZZ a, ZZ n){
+	ZZ r = a-n*(a/n);
+	r = r+(r<0)*n;
+	return r;
 }
+
+ZZ modPow(ZZ a ,ZZ n,ZZ m){
+	
+	ZZ exp (1);
+	ZZ x = module(a,m);
+	
+	while(n>0){
+		if(module(n, ZZ(2)) == 1)
+			exp = module(exp*x,m);
+		x = module(x*x,m);
+		n>>=1;
+	}
+	return exp;
+}
+	
+	
+ZZ RandomNumber(ZZ init,ZZ end) {
+	ZZ k = end - init + 1;
+	ZZ num = RandomBnd(k)+init;
+	return num;
+}
+	
+		
+/*-----------end funciones -----------*/
+		
+		
+		
+
+bool millerRabinTest(ZZ number, ZZ iter)
+{
+	// Casos Base
+	if(module(number,ZZ(2))== 0) return false;
+	if (number <= 1 || number == 4)  return false;
+	if (number <= 3) return true;
+	
+	// number-1 = 2^d * r ; r >= 1
+	ZZ d = number - 1;
+	
+	//Hallar  el exponente d
+	while (module(d,ZZ(2)) == 0)
+		d >>= 1;
+	
+	
+	// Iterar
+	for (ZZ i (0); i < iter; i++){
+		
+		// obtener un numero random en [2..n-2]
+		ZZ a = RandomNumber(ZZ(2),number-2);
+		
+		// X =  a^d mod( n)
+		ZZ x = modPow(a, d, number);
+		
+		if (x == 1  || x == number-1)
+			return true;
+		
+		while (d != number-1)
+		{
+			x= module(x*x,number);
+			//x= x*x % n;
+			d *= 2;
+			
+			if (x == 1)   return false;
+			if (x == number-1)  return true;
+		}
+		return false;
+	}
+	
+	return true;
+}
+		
+		
+		
+		
+int main() {
+	
+
+	
+	cout<<millerRabinTest(ZZ(9882),ZZ(5));
+	
+	cout<<endl;
+	
+}
+			
+			
+			
